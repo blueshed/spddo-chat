53b76201b48fc4ec54bcd0c4d278abd2cee6d6f0
Peter Bunyan <pete@blueshed.co.uk>
2015-09-23 17:16:40 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+version = '0.001'
+
+setup(name='spddo-chat',
+      version=version,
+      packages=find_packages('src',exclude=['spddo.chat.tests*']),
+      include_package_data = True, 
+      exclude_package_data = { '': ['*tests/*'] },
+      install_requires = [
+        'setuptools',
+        'tornado>=4.2',
+        'iron-mq>=0.7',
+      ],
+      entry_points = {
+      'console_scripts' : [
+                           'chat = spddo.chat.server:main'
+                           ]
+      },)
